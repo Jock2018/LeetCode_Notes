1dcb506b9b08d992c71ea7611cd3590e25a0528b
Jock2018 <1377483978@qq.com>
2021-03-31 00:05:53 +0800

    字符串专题：修正错误添加的废弃代码 LeetCode1781

---
+++ LeetCode/基础数据结构/01字符串/LeetCode1781所有子字符串美丽值之和.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+时间：2021/3/30 23:13
+LeetCode原题链接：
+"""
+
+
+class Solution:
+    """解法一：暴力求解
+
+    两层遍历
+    """
+
+    def beautySum(self, s: str) -> int:
+        result = 0
+        for i in range(len(s)):
+            counter = {}
+            for j in range(i, len(s)):
+                counter[s[j]] = counter.get(s[j], 0) + 1
+                max_count = max(counter.values())
+                min_count = min(counter.values())
+                result += max_count - min_count
+        return result
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    s = "aabcb"
+    print(f"预期：5")
+    print(f"实际：{solution.beautySum(s)}")
